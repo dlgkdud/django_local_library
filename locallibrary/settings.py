ad2f701ab32d7167157931b78e6f96f063b59ff2
Your Name <you@example.com>
2020-02-26 16:41:47 +0900

    '-'

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -149,3 +149,6 @@ STATIC_URL = '/static/'
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
+SITE_ID=1
